4c72c03abc7af4fec1292eacbfbea3c77419d80f
naamoonoo <naamoonoo.y@gmail.com>
2019-05-07 11:10:17 -0700

    mem fixed

--- src/memory/ft_memalloc.c
+++ src/memory/ft_memalloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memalloc.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: hnam <hnam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 07:36:27 by hnam              #+#    #+#             */
-/*   Updated: 2019/02/21 07:36:29 by hnam             ###   ########.fr       */
+/*   Updated: 2019/05/07 11:05:55 by hnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	*ft_memalloc(size_t size)
 {
-	char *res;
+	void *res;
 
-	if (!(res = (char *)malloc(size * sizeof(char))))
+	if (!(res = (void *)malloc(size * sizeof(void))))
 		return (NULL);
 	ft_memset(res, 0, size);
 	return (res);
